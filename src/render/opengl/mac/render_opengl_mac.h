116f74238cd884403a2da1197552bca79390a1f4
yuraiz <7516890@gmail.com>
2026-04-14 12:01:59 +0300

    Use objective c on macOS

--- src/render/opengl/mac/render_opengl_mac.h
+++ src/render/opengl/mac/render_opengl_mac.h
@@ -5,12 +5,11 @@
 #define RENDER_OPENGL_LINUX_EGL_H
 
 #define GL_GLEXT_FUNCTION_POINTERS 1
-#include <OpenGL/OpenGL.h>
 #include <OpenGL/gl.h>
 
 // To get proc address
 #include <dlfcn.h>
 
-global id r_ogl_mac_ctx = 0;
+global NSOpenGLContext* r_ogl_mac_ctx = 0;
 
 #endif // RENDER_OPENGL_LINUX_EGL_H
