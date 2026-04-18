c01a01f9ad3c13ddd60a7ebb9db51fc0384ab606
Brett Toomey <brettcwx@gmail.com>
2026-04-18 08:27:31 +0200

    core text backend

--- src/font_provider/font_provider_inc.c
+++ src/font_provider/font_provider_inc.c
@@ -7,6 +7,8 @@
 # include "dwrite/font_provider_dwrite.c"
 #elif FP_BACKEND == FP_BACKEND_FREETYPE
 # include "freetype/font_provider_freetype.c"
+#elif FP_BACKEND == FP_BACKEND_CORE_TEXT
+# include "core_text/font_provider_core_text.c"
 #else
 # error Font provider backend not specified.
 #endif
