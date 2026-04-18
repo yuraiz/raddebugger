6bdc1064e73a9800df809e92436fecfc6b3a00ad
yuraiz <7516890@gmail.com>
2026-04-18 18:44:39 +0300

    Remove no longer needed TODO's

--- src/rdi_from_dwarf/rdi_from_dwarf.c
+++ src/rdi_from_dwarf/rdi_from_dwarf.c
@@ -2788,7 +2788,6 @@ d2r_convert(Arena *arena, D2R_ConvertParams *params)
         path_style = PathStyle_Relative;
       } break;
       case ExecutableImageKind_Macho: {
-        // TODO(yuraiz): Figure that out
         MACH_Bin mach_bin = mach_bin_read_from_file(scratch.arena, params->dbg_name);
         arch       = Arch_arm64;
         image_base = 0;
@@ -3049,7 +3048,6 @@ d2r_convert(Arena *arena, D2R_ConvertParams *params)
     
     // NOTE(yuraiz): Moved to the heap because that variable can blow up the stack.
     RDIM_Type **builtin_types = push_array(scratch.arena, RDIM_Type *, RDI_TypeKind_Count);
-    // RDIM_Type *builtin_types[RDI_TypeKind_Count] = {0};
     for (RDI_TypeKind type_kind = RDI_TypeKind_FirstBuiltIn; type_kind <= RDI_TypeKind_LastBuiltIn; type_kind += 1) {
       RDIM_Type *type = rdim_type_chunk_list_push(arena, &g_d2r_shared.types, D2R_TYPE_CHUNK_CAP);
       type->kind      = type_kind;
