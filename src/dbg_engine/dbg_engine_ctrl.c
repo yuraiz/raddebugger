9fb601f7f1281c5524fdcece72beb3bfee17c6c4
yuraiz <7516890@gmail.com>
2026-05-08 21:55:44 +0300

    Improve basic macOS call stack unwinding

--- src/dbg_engine/dbg_engine_ctrl.c
+++ src/dbg_engine/dbg_engine_ctrl.c
@@ -2886,7 +2886,7 @@ d_unwind_step__pe_x64(D_Handle process_handle, D_Handle module_handle, U64 modul
 }
 
 internal D_UnwindStepResult
-ctrl_unwind_step__mac_arm64(D_Handle process_handle, REGS_RegBlockARM64 *regs, U64 endt_us)
+ctrl_unwind_step__mac_arm64(D_Entity *module, D_Handle process_handle, REGS_RegBlockARM64 *regs, U64 endt_us)
 {
   //////////////////////////////
   // NOTE(yuraiz): From https://developer.apple.com/documentation/xcode/writing-arm64-code-for-apple-platforms
@@ -2895,33 +2895,95 @@ ctrl_unwind_step__mac_arm64(D_Handle process_handle, REGS_RegBlockARM64 *regs, U
   // > As a result, stack traces are always meaningful, even without debug information.
   // 
   // That simplifies unwinding.
+  // 
+  // Usually the standard prologue looks something like that:
+  // sub sp, sp, #0x30
+  // stp fp, lr, [sp, #0x20]
+  // add fp, sp, #0x20
+  //
+  // Currently a function is assumed to be a leaf function if it doesn't contain
+  // `stp` as one of the first instructions. or if it hasn't been executed.
 
-  B32 is_good = true;
-  B32 is_stale = false;
+  Temp scratch = scratch_begin(0, 0);
+
+  // check if we're in a leaf function
+  B32 is_leaf = 0;
+  {
+    // lookup the current procedure in the debug info
+    U64 rip_vaddr = regs->pc.u64;
+    Access *access = access_open();
+    U64 rip_voff = d_voff_from_vaddr(module, rip_vaddr);
+    DI_Key dbgi_key = d_dbgi_key_from_module(module);
+    RDI_Parsed *rdi = di_rdi_from_key(access, dbgi_key, 0, 0);
+    RDI_Symbol *procedure = rdi_procedure_from_voff(rdi, rip_voff);
+    U64 addr = rdi_first_voff_from_procedure(rdi, procedure);
+    
+    // disassemble a few instructions
+    U64 instr_count = 4;
+    U64 instr_addr = module->vaddr_range.min + addr;
+    Rng1U64 vrange = {instr_addr, instr_addr + sizeof(U32) * instr_count};
+    String8 data = dmn_process_read_block(scratch.arena, process_handle.dmn_handle, vrange);
+
+    // search for the first stp instruction
+    U64 stp_vaddr = 0;
+    for(U64 off = 0; off < data.size;)
+    {
+      DASM_Inst inst = dasm_inst_from_code(scratch.arena, Arch_arm64, 0, str8_skip(data, off), DASM_Syntax_Intel);
+
+      if(str8_match_lit("stp", str8_prefix(inst.string, 3), 0))
+      {
+        stp_vaddr = instr_addr + off;
+      }
+
+      if(inst.size == 0 || stp_vaddr != 0 || inst.flags & DASM_InstFlag_Return)
+      {
+        break;
+      }
+
+      off += inst.size;
+    }
+
+    // "leaf" if we correctly got the debug info and stp wasn't found / not yet executed
+    is_leaf = procedure != 0 && (stp_vaddr == 0 || stp_vaddr > rip_vaddr);
+  }
+
+  B32 is_good = 1;
+  B32 is_stale = 0;
 
   U64 fp = regs->fp.u64;
 
   // validate fp
   if ((fp == 0) || (!(((fp) & 0xf) == 0)))
   {
-    is_good = false;
+    is_good = 0;
   }
   
-  U128 stack_frame = {0};
-  if(is_good && !d_process_memory_read_struct(process_handle, fp, &is_stale, &stack_frame, endt_us))
+  if(is_leaf)
   {
-    is_good = false;
+    // frameless unwinding
+    regs->pc.u64 = regs->lr.u64;
+    regs->sp.u64 = fp;
   }
-
-  // NOTE(yuraiz): Return addresses on stack signed by arm64e ABI.
-  // but I don't think that should be supported in the debugger.
-  regs->fp.u64 = stack_frame.u64[0];
-  regs->pc.u64 = stack_frame.u64[1];
-  regs->sp.u64 = fp;
+  else
+  {
+    // frame-based unwinding
+    U128 stack_frame = {0};
+    if(is_good && !d_process_memory_read_struct(process_handle, fp, &is_stale, &stack_frame, endt_us))
+    {
+      is_good = 0;
+    }
+    
+    // NOTE(yuraiz): Return addresses on stack signed by arm64e ABI.
+    // but I don't think that should be supported in the debugger.
+    regs->fp.u64 = stack_frame.u64[0];
+    regs->pc.u64 = stack_frame.u64[1];
+    regs->sp.u64 = fp;
+  }
 
   D_UnwindStepResult result = {0};
   if(!is_good) {result.flags |= D_UnwindFlag_Error;}
   if(is_stale) {result.flags |= D_UnwindFlag_Stale;}
+  scratch_end(scratch);
   return result;
 }
 
@@ -3026,7 +3088,7 @@ d_unwind_from_thread(Arena *arena, D_EntityCtx *ctx, D_Handle thread, U64 endt_u
             default:{}break;
             case Arch_arm64:
             {
-              step_result = ctrl_unwind_step__mac_arm64(process_entity->handle, regs_block, endt_us);
+              step_result = ctrl_unwind_step__mac_arm64(module_entity, process_entity->handle, regs_block, endt_us);
             }break;
           }
         }break;
